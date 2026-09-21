21c38fe26374af4b2bbd4dc16c4ab5e9ee7b6994
Have fun with your bytes. <worldofliu@gmail.com>
2026-09-21 19:26:19 +0800

    ss-libev: restore libev config generation

--- luci-app-passwall/luasrc/passwall/util_shadowsocks.lua
+++ luci-app-passwall/luasrc/passwall/util_shadowsocks.lua
@@ -21,7 +21,11 @@ function gen_config_server(node)
 		config.server = {"[::0]", "0.0.0.0"}
 	end
 
-	if node.type == "SSR" then
+	if node.type == "SS" then
+		config.plugin = plugin_file or nil
+		config.plugin_opts = (plugin_file) and node.plugin_opts or nil
+		config.mode = mode
+	elseif node.type == "SSR" then
 		config.protocol = node.protocol
 		config.protocol_param = node.protocol_param
 		config.obfs = node.obfs
